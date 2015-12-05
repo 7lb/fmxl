24e892b26023d1b00014992594a6c9ead2174beb
Luca Benci <benci.luca@gmail.com>
2015-12-05 17:58:47 +0100

    Minified files are now named min.filename.css

--- minify.sh
+++ minify.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/bash
-for f in build/*.css;
+build_dir="build"
+for f in $(basename -a $build_dir/[^min]*.css);
 do
-    node_modules/csso/bin/csso $f ${f/%css/min.css};
+    node_modules/csso/bin/csso $build_dir/$f $build_dir/"min."$f;
 done
