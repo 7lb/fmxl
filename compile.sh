eb93ae134e2e0a51bbb65d554ecafdc0ae8d6b10
Luca Benci <benci.luca@gmail.com>
2015-12-05 16:22:49 +0100

    Optimized CSS with csso

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/bash
-sassc base/base.scss build/base.css &
-sassc docs/docs.scss build/docs.css &
-sassc docs/assassin_dark_docs.scss build/docs_dark.css &
-sassc docs/assassin_light_docs.scss build/docs_light.css &
-sassc forum/fmxl.scss build/forum.css &
-sassc forum/assassin_dark_forum.scss build/forum_dark.css &
-sassc forum/assassin_light_forum.scss build/forum_light.css
+sassc base/base.scss | node_modules/csso/bin/csso -o build/base.css &
+sassc docs/docs.scss | node_modules/csso/bin/csso -o build/docs.css &
+sassc docs/assassin_dark_docs.scss | node_modules/csso/bin/csso -o build/docs_dark.css &
+sassc docs/assassin_light_docs.scss | node_modules/csso/bin/csso -o build/docs_light.css &
+sassc forum/fmxl.scss | node_modules/csso/bin/csso -o build/forum.css &
+sassc forum/assassin_dark_forum.scss | node_modules/csso/bin/csso -o build/forum_dark.css &
+sassc forum/assassin_light_forum.scss | node_modules/csso/bin/csso -o build/forum_light.css
